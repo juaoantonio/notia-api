c08530ce9391df0ce01a3eb95ca3173aa20cb859
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-08-15 07:13:54 -0300

    refactor: remove `defineRoutes` utility and unused imports from codebase

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-import type { DefineRoutesHandler, FastifyTypedInstance } from './types';
-
-export function defineRoutes(handler: DefineRoutesHandler) {
-  return (app: FastifyTypedInstance, _: Record<never, never>, done: () => void) => {
-    handler(app);
-    done();
-  };
-}
