88e51d05ff99794e8c2517984e8e3b47276ddab1
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-08-16 08:20:27 -0300

    feat: enable query logging in Prisma based on environment configuration

--- src/providers/prisma.provider.ts
+++ src/providers/prisma.provider.ts
@@ -1,6 +1,7 @@
 import fp from 'fastify-plugin';
 import { PrismaClient } from '@/generated/prisma';
 import type { FastifyTypedInstance } from '@/types';
+import { env } from '@config/env';
 
 declare module 'fastify' {
   interface FastifyInstance {
@@ -10,7 +11,14 @@ declare module 'fastify' {
 
 export default fp(
   async (app: FastifyTypedInstance) => {
-    const prisma = new PrismaClient();
+    const prisma = new PrismaClient({
+      log: [env.NODE_ENV === 'development' ? 'query' : null].filter(Boolean) as (
+        | 'query'
+        | 'info'
+        | 'warn'
+        | 'error'
+      )[],
+    });
     await prisma.$connect();
 
     app.decorate('prisma', prisma);
