a5fd8644339d0f12681ed8d5b1dd4631ff60faca
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-08-22 17:21:24 -0300

    feat: log server details on startup
    - Added a log to display the server URL and environment when the application starts.

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,5 @@ import { buildApp } from './app';
 import { env } from '@config/env';
 
 const server = await buildApp();
+server.log.info(`Server is running at http://${env.HOST}:${env.PORT} in ${env.NODE_ENV} mode`);
 await server.listen({ port: env.PORT, host: env.HOST });
