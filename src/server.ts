90022821f2b5c8d0d6bf16570586166a173a37dc
João Antônio Barbosa <joaobarbosadev@gmail.com>
2025-08-26 13:30:41 -0300

    fix: remove unnecessary await from buildApp() in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { buildApp } from './app';
 import { env } from '@/config/env';
 
-const server = await buildApp();
+const server = buildApp();
 server.log.info(`Server is running at http://${env.HOST}:${env.PORT} in ${env.NODE_ENV} mode`);
 await server.listen({ port: env.PORT, host: env.HOST });
